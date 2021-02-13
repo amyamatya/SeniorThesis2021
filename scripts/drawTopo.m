72b9890c4cc25a893db063db6ea532d96352b72b
Amy Amatya <aamatya@dynamic-oit-vapornet100-10-9-86-49.princeton.edu>
2021-02-13 15:56:00 -0500

    fix

--- scripts/drawTopo.m
+++ scripts/drawTopo.m
@@ -1,42 +1,73 @@
-function [fig] = drawTopo()
-% Plot a worldmap with bathymetry, plate boundaries, and SACV station
+function [fig] = drawTopo(bound)
+% Draw a worldmap with relief and plate boundaries
+% Plate boundary source: http://www-udc.ig.utexas.edu/external/plates/data.htm
 % Last modified 2/12/21 @aamatya
-clf
-% Station coordinates
+
+%---Input variables------------
+% bound     - 1 = boundaries colored by type
+%           - 2 = all boundaries white
+%           - default = no boundaries
+
+% Load Cape Verde coordinates
 sacvLat = 14.97;
 sacvLon = convertLon(-23.608, '-180to360');
-% Plate boundaries
-plateKml = unzip('plate-boundaries.kmz');
-[pbxOld, pby, pbz] = read_kml(string(plateKml(4)));
-errs = find(pbz ~= 0);
-pbxOld(errs) = []; pby(errs) = []; pbz(errs) = [];
-pbx = convertLon(pbxOld, '-180to360');
-% Topography
+% Load plate boundaries
+if exist('bound','var')
+    div = shaperead('ridge.shp');
+    divLon = [div.X]; divLat = [div.Y];
+    divLon(isnan(divLon)) = []; divLat(isnan(divLat)) = [];
+    conv = shaperead('trench.shp');
+    convLon = [conv.X]; convLat = [conv.Y];
+    convLon(isnan(convLon)) = []; convLat(isnan(convLat)) = [];
+    trans = shaperead('transform.shp');
+    transLon = [trans.X]; transLat = [trans.Y];
+    transLon(isnan(transLon)) = []; transLat(isnan(transLat)) = [];
+end
+% Load topography
 [topoLat, topoLon, topoZ] = makeTopo();
-% Shorelines
+% Load shorelines
 filename = gunzip('gshhs_c.b.gz', tempdir);
 shorelines = gshhs(filename{1});
 delete(filename{1})
 levels = [shorelines.Level];
-land = (levels == 1);  
-% Show worldmap
+land = (levels == 1);
+% Show on worldmap
 clf
 hold on
-ax = worldmap([-90 90],[160 159]);
+ax = worldmap([-80 80],[180 159]);
 geoshow(shorelines(land), 'facecolor', [0.4 0.9 0.4],'facealpha',0);
 scatterm(topoLon(1:20:end), topoLat(1:20:end),1, topoZ(1:20:end));
 demcmap([min(topoZ) max(topoZ)]);
-colorbar
-scatterm(pby, pbx, 0.06, pbz); %'w.');
-scatterm(sacvLat, sacvLon, 10,'filled','ro');
-textm(sacvLat + 10, sacvLon - 10, 'CV','Color','r');
+if exist('bound','var')
+    if bound == 1
+        div = scatterm(divLat, divLon, 1, 'r.');
+        conv = scatterm(convLat, convLon, 1, 'y.');
+        trans = scatterm(transLat, transLon, 1, rgb('LightPink'),'.');
+        [lgnd,obj] = legend([div conv trans], 'Divergent','Convergent',...
+            'Transform','location','southeast');
+        set(lgnd.BoxFace, 'ColorType','truecoloralpha', 'ColorData',uint8(255*[.5;.5;.5;.5]));
+        objhl = findobj(obj, 'type', 'Patch');
+        set(objhl, 'Markersize', 15);
+    elseif bound == 2
+        scatterm([divLat convLat transLat], [divLon convLon transLon], 1, 'w.');
+    end
+end
+scatterm(sacvLat, sacvLon, 10,'filled','wo');
+textm(sacvLat + 10, sacvLon - 10, 'CV','Color','w');
 axis image
 setm(ax, 'MlabelParallel', 'south');
 hold off
-delete('usgs.jpg','PlateMotionLegend.png','doc.kml','arrow.png');
 fig = gcf;
 end
 
+% USGS plate boundaries
+% plateKml = unzip('plate-boundaries.kmz');
+% [pbxOld, pby, pbz] = read_kml(string(plateKml(4)));
+% errs = find(pbz ~= 0);
+% pbxOld(errs) = []; pby(errs) = []; pbz(errs) = [];
+% pbx = convertLon(pbxOld, '-180to360');
+% scatterm(pby, pbx, 0.06, pbz); %'w.');
+% delete('usgs.jpg','PlateMotionLegend.png','doc.kml','arrow.png');
 
 
 
--- scripts/figure01.m
+++ scripts/figure01.m
@@ -1,57 +1,27 @@
-%% figure01: Map of geographic/tectonic context
-% Last modified 11/15/2020 by aamatya@princeton.edu
-%% 1 - Load, reconfigure, save topographic relief data
-% (Skip to section 2)
-% Data: ETOPO1 Ice Surface from https://www.ngdc.noaa.gov/mgg/global/global.html
-[topoxOld, topoyOld, topozOld] = grdread2('ETOPO1_Ice_g_gdal.grd');
-[lats, lons] = size(topozOld);
-topoxOld = convertLon(topoxOld, '-180to360');
-topox = repmat(topoxOld, lats, 1);
-topoy = repmat(topoyOld', 1, lons);
-topox = topox(:); topoy = topoy(:); topoz = double(topozOld(:));
-save('topoData','topox','topoy','topoz');
-clear topoxOld topoyOld topozOld lats lons
-%% 2 - Load other geographic data
-% Load topoData.mat
-% load topo60c
-load('topoData.mat');
-% Load shorelines
-filename = gunzip('gshhs_c.b.gz', tempdir);
-shorelines = gshhs(filename{1});
-delete(filename{1})
-levels = [shorelines.Level];
-land = (levels == 1);  
-% Load and filter plate boundaries
-[pbxOld, pby, pbz] = read_kml('plate-boundaries.kml');
-errs = find(pbz ~= 0);
-pbxOld(errs) = []; pby(errs) = []; pbz(errs) = [];
-pbx = convertLon(pbxOld, '-180to360');
-% Store SACV coordinates
-sacvLat = 14.97;
-sacvLon = convertLon(-23.608, '-180to360');
-%% Plot map
-close all
+%% figure01: topographic/plate boundary map of Cape Verde
+% Last modified 2/13/21 by aamatya@princeton.edu
+
 figure(1)
 hold on
-ax = worldmap([-90 90],[160 159]);
-% geoshow(topo60c,topo60cR,'DisplayType','texturemap');
-% colormap(winter);
-scatterm(topoy(1:10:end), topox(1:10:end),1, topoz(1:10:end));
-demcmap([min(topoz) max(topoz)]);
-
-geoshow(shorelines(land), 'facecolor', [0.4 0.9 0.4],'facealpha',0);
-title('SACV Seismic Context');
-xlabel('Latitude');
-ylabel('Longitude');
-setm(ax,'mlabelparallel',-90);
-% Plot data
-plates = scatterm(pby, pbx, 0.06, 'w.');
-
-scatterm(sacvLat, sacvLon ,20, 'filled','ro');
-annotation('textbox',[.5 .57 .07 .04],'String','SACV','edgecolor','r','color','r');
+drawTopo(1);
+title('Cape Verde and Plate Boundaries');
 hold off
-axis image
-% print(figure(1), '/Users/aamatya/Desktop/Thesis/Figures/figure01','-dpdf');
+print(gcf, '/Users/aamatya/Documents/MATLAB/ST2021/figures/figure01','-dpdf');
 
-% Clean workspace
-% clearvars -except filename land levels shorelines sacvLat sacvLon
+% Draw topo and plate maps in same figure
+% clfall
+% figure(1);
+% drawTopo();
+% colorbar;
+% figure(2);
+% drawPlates();
+% figlist=get(groot,'Children');
+% figlist = flip(figlist);
+% newfig=figure;
+% tcl=tiledlayout(newfig,'flow');
+% for i = 1:numel(figlist)
+%     figure(figlist(i));
+%     ax=gca;
+%     ax.Parent=tcl;
+%     ax.Layout.Tile=i;
+% end

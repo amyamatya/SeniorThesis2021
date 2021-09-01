e3358424a0fa2e4f2e4481fe998dfe49e2dcde3e
Amy Amatya <43310546+amyamatya@users.noreply.github.com>
2021-08-31 23:21:35 -0400

    Add files via upload

---
+++ startup.m
@@ -0,0 +1,38 @@
+%% Let's get it started!
+% Last modified 11/15/2020 by aamatya@princeton.edu
+addpath /Users/aamatya/Documents/MATLAB/ST2021/scripts/plots
+addpath /Users/aamatya/Documents/MATLAB/ST2021/scripts
+% Path to irisFetch
+addpath /Users/aamatya/Documents/MATLAB/ST2021/files/MFILES
+addpath /Users/aamatya/Documents/MATLAB/ST2021/files/MFILES/irisFetch-matlab-2.0.11
+% Path to Java jar files for irisFetch
+javaaddpath /Users/aamatya/Documents/MATLAB/ST2021/files/JFILES
+javaaddpath /Users/aamatya/Documents/MATLAB/ST2021/files/JFILES/IRIS-WS-2.0.19.jar
+% Path to Java jar files for mattaup
+javaaddpath /Users/aamatya/Documents/MATLAB/ST2021/files/JFILES/seisFile-1.8.0.jar
+javaaddpath /Users/aamatya/Documents/MATLAB/ST2021/files/JFILES/TauP-2.4.5.jar
+javaaddpath /Users/aamatya/Documents/MATLAB/ST2021/files/JFILES/MatTauP-2.1.1.jar
+% Path to functions
+addpath /Users/aamatya/Documents/MATLAB/ST2021/functions
+addpath /Users/aamatya/Documents/MATLAB/ST2021/functions/read_kml
+addpath /Users/aamatya/Documents/MATLAB/ST2021/functions/suplabel
+addpath /Users/aamatya/Documents/MATLAB/ST2021/functions/rgb
+addpath /Users/aamatya/Documents/MATLAB/ST2021/functions/grdread2
+addpath /Users/aamatya/Documents/MATLAB/ST2021/functions/slepian_oscar
+addpath /Users/aamatya/Documents/MATLAB/ST2021/functions/slepian_alpha
+addpath /Users/aamatya/Documents/MATLAB/ST2021/functions/ds2nfu
+addpath /Users/aamatya/Documents/MATLAB/ST2021/functions/patchline
+addpath /Users/aamatya/Documents/MATLAB/ST2021/functions/rflexa/matlab
+addpath /Users/aamatya/Documents/MATLAB/ST2021/functions/rflexa/matlab/external
+addpath /Users/aamatya/Documents/MATLAB/ST2021/functions/contourCodes
+
+% Path to data
+addpath /Users/aamatya/Documents/MATLAB/ST2021/files
+addpath /Users/aamatya/Documents/MATLAB/ST2021/files/PLATES_PlateBoundary_ArcGIS
+addpath /Users/aamatya/Documents/MATLAB/ST2021/files/NnrMRVL_PltBndsLatLon
+% Other fixes
+set(0,'DefaultLegendAutoUpdate','off')
+% Prevent Matlab from opening
+% com.mathworks.services.Prefs.setBooleanPref('EditorGraphicalDebugging',false)
+
+
